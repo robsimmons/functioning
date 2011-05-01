5eba52a8e697f78a4b4960fb4226572cbd6b33cb
Rob Simmons <robsimmons@gmail.com>
2011-05-01 15:19:12 -0400

    Move Explat the Robot into graphics for fully functional OSX build process
    --HG--
    rename : functioning/OSX/Resources/Explat.icns => media/graphics/ExplatTheRobot.icns

--- functioning/plistgen.sh
+++ functioning/plistgen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat <<EOF
+cat >/tmp/Func.app/Contents/Info.plist <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
